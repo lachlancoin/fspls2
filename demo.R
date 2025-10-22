8406db733a16323feec2762c826e56b8f5c12eef
Lachlan Coin <lcoin@5260l-219884-l.mobility.unimelb.net.au>
2025-10-22 14:40:54 +1100

    update

--- demo.R
+++ demo.R
@@ -1,8 +1,6 @@
 
 ##SET APPROPRIATE LIB PATHS 
-
-##SHOULD BE RUN FROM WHERE GIT CLONED TO
-.libPaths("~/R/x86_64-pc-linux-gnu-library/4.1/")
+#.libPaths("~/R/x86_64-pc-linux-gnu-library/4.1/")
 options(bigmemory.allow.dimnames=TRUE)
 library(jsonlite)
 library(R6)
@@ -12,148 +10,99 @@ library(tidyr)
 library(MASS);
 library(ggplot2)
 library(pROC); 
-library(nnet)  ## for multinomial
-library("binom") ## for plotting
+library(binom) ## for plotting
 library(confintr)
 
 ##to store signatures
 library(DBI); 
 library(RSQLite);
-
 library(cowplot)
-
 library(bigmemory)
 
-#library(wCorr)
-##LOAD CODE
+
 ##SHOULD RUN FROM FSPLS2 directory
 if(rev(strsplit(getwd(),"/")[[1]])[1]!="fspls2")stop("not in right directory")
 src1=grep(".R$",dir("./R",rec=T),v=T)
-
 invisible(try(lapply(paste("./R",src1,sep="/"), function(x) {print(x);source(x)})))
 
+## LOAD EXAMPLE DATASETS - USE RDS
 if(FALSE){
-rdats = grep("rdat",dir("data",full=T),v=T)
-examples = lapply(rdats, function(rdat){
-  load(rdat)
-  .convert(data, max=nrow(data$data),factor=length(grep("multi", rdat))>0)
-})
-names(examples)=lapply(rdats, function(str)strsplit(str,"/")[[1]][2])
+  ##LOAD RDAT DATA, BUT THIS HAS NOT BEEN INCLUDED IN REPO
+  rdats = grep("rdat",dir("data",full=T),v=T)
+  examples = lapply(rdats, function(rdat){
+    load(rdat)
+    .convert(data, nme = rdat, max=nrow(data$data),factor=length(grep("multi", rdat))>0)
+  })
+  names(examples)=lapply(rdats, function(str)strsplit(str,"/")[[1]][2])
 }else{
-## LOAD EXAMPLE DATASETS
-example_files= grep("_data.rds",dir("data", full=T),v=T)
-names(example_files) = lapply(example_files, function(x)strsplit(x,"/")[[1]][2])
-examples = lapply(example_files, readRDS)
+  example_files= grep("_data.rds",dir("data", full=T),v=T)
+  names(example_files) = lapply(example_files, function(x)strsplit(x,"/")[[1]][2])
+  examples = lapply(example_files, readRDS)
+  for(k in 1:length(examples)) examples[[k]]$nme = names(examples)[[k]]
 }
-datasets =examples[4]; pthresh = 0.05 ; randomise=F; duplicate=F
-options("fspls.types"=
-          fromJSON('{"gaussian": ["correlation","rms"],"binomial":"AUC","multinomial":"AUC","ordinal" : "AUC_all"}'))
+
+
+
+##HOW TO EVALUATE DIFFERENT DATASETS
+options("fspls.types"= fromJSON('{"gaussian": ["correlation","rms"],"binomial":["AUC","area","max_diff","max_diff_x"],"multinomial":"AUC","ordinal" : "AUC_all"}'))
+
+#SET UP TRANSFORMATIONS
 pows = c(0.5,1,1.5)
-pows = seq(0.1,1.9,by=.1)
-pows = 1
-transform_y=getYTransform(pow = pows, offset=0.1, norm=1000, n_random=5)
-#transform_y=list(x=c("function(y) y","function(y) y"),exp= c("function(y) sign(y)*y^2", "function(y) sign(y)*abs(y)^(1/2)"))
+transform_y=getYTransform(pow = pows, offset=0.1, norm=1000, n_random=10)
 
-runAll<-function(datasets, randomise=F,pthresh = 0.001, duplicate=F,
-                 transform_y = list(x=c("function(y) y","function(y) y"), rand=c("function(y) randomize(y)", "function(y) randomize(y)"))){
-  y = datasets[[1]]$y
-  flags = list(pthresh = pthresh, max=10,nrep=1,batch=0,topn=20,beam=1,all_v_all=F,  project=T,
-               stop_y="rand",x_transform=T,
-               transform_y = toJSON(transform_y),
-               useoffset=T,useglmnet=T#quantiles = toJSON(c(0,0.1))
-              )
-  
-#  flags[['transform']] =toJSON(getXTransform(c(seq(-1,-0.2,by=0.2),seq(0.1,0.9,by=.1), seq(1,2,by=.5))))#  '{"x" :"function(x) x", "log":"function(x) log1p(x)"}'
-  
+#SET UP FLAGS
+flags = list(pthresh = 0.5, max=10,nrep=10,batch=0,topn=20,beam=1,all_v_all=F,  project=T,  stop_y="rand",x_transform=T,
+             transform_y = toJSON(transform_y), useoffset=T,useglmnet=T
+)
 
-  datasAll =datasEnv$new(datasets,flags=flags) 
-  datasAll$updateTransforms(toJSON(transform_y))
-  #sigs = datasAll$getSigDB(nme1="combined",reload=T)
-  
-  #sigs$clear_all()
-  phens=datasAll$pheno()$all
-  options("x_transform"="NA")
-  ## FIND VARIABLES
-  vars_all = datasAll$select(phens, flags, verbose=T)
-  
+datasets =examples[3];
+#runAll<-function(datasets){
+  datasH = lapply(datasets, function(d)dataH$new(d,nme=d$nme, flags=flags))
+  nmesH = names(datasH); names(nmesH) = nmesH
+  datasAll =datasEnv$new(datasH,flags=flags) 
+
+  phens=datasAll$pheno()
+  vars_all = datasAll$select(phens$all, flags, verbose=F,useDB=T)
   vars_all1=.extractFullVars(vars_all)
-  #sigs$saveVars(vars_all)
- # sigs$experiments()
+  all_models = lapply(nmesH, function(nmeh){
+    dh=datasAll$datasH[[nmeh]]
+   am=dh$makeAllModels(vars_all,useDB=T)
+  })
+  eval1= .merge1_new(lapply(nmesH, function(nmeh){
+    all_modelsh = all_models[[nmeh]]
+    dh = datasAll$datasH[[nmeh]]
+    dh$evaluateAllModels(all_modelsh, useDB=F)
+  }), addName="data")
   
-#  options("fspls.verbose1"=T)
-  
-  all_models = datasAll$makeAllModels(vars_all,flags=flags)
-#  sigs$saveModels(all_models)
-  #all_models1 = sigs$loadModels(flags, phens ,"")
-  eval1 = datasAll$evaluateAllModels(all_models)
-  
- # sigs$saveEval(eval0, flags, phens, "")
-  #eval1 = sigs$loadEval(flags,phens,"")
-  #eval = subset(eval, model!="avg")
   ggps1=.plotEval2(eval1,legend=T, grid1="subpheno", grid0="measure",
                    shape_color=c("data","transf"),sep_by=c("cv_full"), showranges=T,
                    scales="free",title =names(phens)[1], title1="pheno" ) #, grid="pheno~cv_full",showranges = F)
-  if(TRUE) return(ggps1)
-  ggps1$`CV=avg`
-  ggps1$`CV= FALSE FULL= TRUE`
-  if(FALSE){ ## show cum plots
-    predictions0 =datasAll$extractPredictions(all_models, CV = F, liab=F, data_nme = names(datasAll$datas)[[1]]);
-    fam=datasAll$datas[[1]]$family[[1]]
+  pdf("output.pdf")
+  lapply(ggps1, print)
+  dev.off()
+  
+  if(TRUE){ ## show cumulative plots for calibration
+    ##extract all predictions
+    predictions0 = lapply(nmesH, function(nmeh){
+      dh=datasAll$datasH[[nmeh]]
+      am=dh$extractPredictions(all_models[[nmeh]], CV = F, liab=F);
+    })
+      
+    fam=datasAll$datasH[[1]]$data$family[[1]]
     area_p = .getAreaPlot1(predictions0,families=fam)
     ###area_p0$value= funcstr1(area_p0$value) only for transformed values, should put this in extractPredictions
     #aa=roc(predictions[[2]]$y, predictions[[2]]$X0)
     ggp_pred0=.plotArea(area_p, rename=F)
     ggp_pred0
-     # predictions0 =datasAll$extractPredictions(all_models,phens, flags, CV = F, liab=F, data_nme = names(datasAll$datas)[[1]]);
-    #  ggp_pred0=.plotArea1(predictions0, rename=T,max_vars=44)
-    #  ggp_pred0
   }
-  if(is.null(eval)) return (NULL)
-  eval3 = .calcEval1(eval0)
-  ggps = .plotEval1(eval3,  legend=T)
-#for multinomial or ordinal
-#ggps = .plotEval1(eval, rename=F,grid="subpheno~cv")
-ggps
-if(TRUE) return(ggps)
+#}
 
-
-##VISUALISE PREDICTIONS
-#predictions =datas$extractPredictions(all_models,phens, flags, CV = F);
-#aa=roc(predictions[[2]]$y, predictions[[2]]$X0)
-#.plotArea(predictions, rename=F,datas$datas[[1]]$family[1])
-
-
-varnames = variables$variables[[length(variables$variables)]]
-projOut=datasAll$getProjectedData(varnames)
-variance_before = datasAll$getVariance()
-variance_after =   lapply(projOut, function(p1){
-  lapply(p1, function(p2){
-    sparse_variance(p2)
-  })
-})
-
-##PRINT VARIANCE BEFORE AND AFTER
-varnames1 = unlist(lapply(varnames, function(v)match(v[[1]], names(datasAll$datas[[1]]$data))))
-for(j in 1:length(datasAll$datas)){
-  for(k in 1:length(datasAll$datas[[j]]$data)){
-   varnames2 = varnames[varnames1==k]
-    inds = match(unlist(lapply(varnames2, function(v)v[2])),colnames(datasAll$datas[[j]]$data[[k]]))
-    print("before")
-    df = data.frame(cbind(variance_before[[j]][[k]][inds],variance_after[[j]][[k]][inds]))
-    names(df) = c("before","after")
-    print(df)
-  }
-}
-
-}
-
-ggp_all =list()
 #examples = examples[grep("multinomial", names(examples), inv=T)] # broken for multinomail
-for(i in 3:length(examples)){  
-  print(i)
-  ggp_all[[i]] = runAll(examples[i], randomise=F, pthresh = 0.0001)
-}
-plot_grid(ggp_all[[1]][[1]], ggp_all[[2]][[1]], ggp_all[[3]][[1]],ggp_all[[4]][[1]])
-names(ggp_all) = names(examples)
+#for(i in 1:length(examples)){  
+#  print(i)
+#  ggp_all[[i]] = runAll(examples[i], randomise=F, pthresh = 0.0001)
+#}
+#plot_grid(ggp_all[[1]][[1]], ggp_all[[2]][[1]], ggp_all[[3]][[1]],ggp_all[[4]][[1]])
+#names(ggp_all) = names(examples)
 #runAll(datasets[1])
 
