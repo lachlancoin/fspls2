51d3b4f60d199b4c51fca3f34d4c0ed6ca5cde8a
Lachlan Coin <lcoin@5260l-219884-l.mobility.unimelb.net.au>
2026-01-28 17:40:09 +1100

    update

--- nitric_example.R
+++ nitric_example.R
@@ -34,7 +34,8 @@ setwd("~/Data/Jingni")  ## where data is located
 .slug<-function(str){
   gsub("-","_",str)
 }
-
+ids = data.table::fread("ids_discovery_and_validation.csv.gz")
+ids$Sequencing_Sample_ID = .slug(ids$Sequencing_Sample_ID)
 meta_data = data.table::fread("NITRIC_Metadata_09112023.csv.gz")
 comp_index = match("comp_outcome",names(meta_data) )
 
@@ -46,6 +47,9 @@ counts = t(counts0[,-1]) ## so that transcripts are in columns.  Removes first c
 dimnames(counts)= list(sample_names, gene_names)
 meta_data$Sequencing_Sample_ID = .slug(meta_data$Sequencing_Sample_ID)
 mi1 = match(sample_names,meta_data$Sequencing_Sample_ID)
+mi2 = match(sample_names, ids$Sequencing_Sample_ID)
+
+ids = ids[mi2[!is.na(mi2)],,drop=F]
 meta_data = meta_data[mi1[!is.na(mi1)],,drop=F];
 rownames(meta_data) = meta_data$Sequencing_Sample_ID
 counts = counts[!is.na(mi1),,drop=F]
@@ -54,47 +58,69 @@ subind = which(dimnames(meta_data)[[2]] %in% c("comp_outcome"))[1]
 y=as.matrix(meta_data[,..subind,drop=F])
 rownames(y) = rownames(meta_data)
 
-nitric_data = list(nme="NITRIC", dataset = list(rna = counts), y=y)
+
+discovery = which(ids$validation==0)
+validation = which(ids$validation==1)
+
+
+
+
+
+###optional normalisation
+#counts = apply(counts, c(1,2), function(x) log(x+0.1))
+
+nitric_data = list(nme="NITRIC", dataset = list(rna = counts[discovery,,drop=F]), y=y[discovery,,drop=F])
+nitric_validation= list(nme="NITRIC", dataset = list(rna = counts[validation,,drop=F]), y=y[validation,,drop=F])
 
 
 
 
 
 ## NOW RUN FSPLS
-pows = c(0.5,1,1.5)
+#pows = c(0.5,1,1.5,2.0)
+pows = c(1)
 transform_y=getYTransform(pow = pows, offset=0.1, norm=1000, n_random=10)
 
 #SET UP FLAGS
-flags = list(pthresh = 0.05, max=10,nrep=10,batch=0,topn=10,beam=1,all_v_all=F,  project=T,  stop_y="rand",x_transform=T,
+flags = list(pthresh = 0.05, max=10,nrep=1,batch=0,topn=20,beam=1,all_v_all=F,  project=T,  stop_y="rand",x_transform=T,
              pheno_balance = T,transform_y = toJSON(transform_y), useoffset=T,useglmnet=T,loadPV=T
              
 )
 
+flags$quantiles = '[0.5]' 
 
 datasets = list(NITRIC=nitric_data)
+datasets_val = list(validation = nitric_validation);
 
 
 #  flags[['transform']] =toJSON(getXTransform(c(seq(-1,-0.2,by=0.2),seq(0.1,0.9,by=.1), seq(1,2,by=.5))))#  '{"x" :"function(x) x", "log":"function(x) log1p(x)"}'
 
 
 datasAll =datasEnv$new(datasets,flags=flags, hasNA=F) 
+datasAll_validation =datasEnv$new(datasets_val,flags=flags, hasNA=F) 
+
 datasAll$updateTransforms(toJSON(transform_y))
 phens=datasAll$pheno()$all
 options("x_transform"="NA")
 ## FIND VARIABLES
 vars_all = datasAll$select(phens, flags, verbose=T)
-vars_all1=.extractFullVars(vars_all)
+#vars_all1=.extractFullVars(vars_all)
 
 
 all_models = datasAll$makeAllModels(vars_all,flags=flags)
+
+
 #  sigs$saveModels(all_models)
 #all_models1 = sigs$loadModels(flags, phens ,"")
 eval1 = datasAll$evaluateAllModels(all_models)
 
+eval_validation = datasAll_validation$evaluateAllModels(all_models)
+
+
 # sigs$saveEval(eval0, flags, phens, "")
 #eval1 = sigs$loadEval(flags,phens,"")
 #eval = subset(eval, model!="avg")
-ggps1=.plotEval2(eval1,legend=T, grid1="subpheno", grid0="measure",
+ggps1=.plotEval2(eval_validation,legend=T, grid1="subpheno", grid0="measure",
                  shape_color=c("data","transf"),sep_by=c("cv_full"), showranges=T,
                  scales="free",title =names(phens)[1], title1="pheno" ) #, grid="pheno~cv_full",showranges = F)
 ggps1
